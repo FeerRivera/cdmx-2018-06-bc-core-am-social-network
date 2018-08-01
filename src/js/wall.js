c5619accf8728454bc80f1a1897de01d7a303b50
FeerRivera <39814132+FeerRivera@users.noreply.github.com>
2018-08-01 03:34:58 -0500

    se checo la indentacion

--- src/js/wall.js
+++ src/js/wall.js
@@ -14,64 +14,66 @@ let profileImage = document.getElementById("photo-user");
 let runUser;
 
 const postCommit = () => {
-  newPost.addEventListener('click', saveData = () =>{
-    const actUser = firebase.auth().currentUser;
-    const postTxt = inputTxt.value;
-    if (postTxt == '') {
+    newPost.addEventListener('click', saveData = () => {
+        const actUser = firebase.auth().currentUser;
+        const postTxt = inputTxt.value;
+        if (postTxt == '') {
 
-    } else {
-      const userKey = firebase.database().ref().child('Mensajes').push().key;
-      let update = {
-        user: actUser.uid,
-        userName: actUser.displayName,
-        post: postTxt
-      };
-      firebase.database().ref(`Mensajes/${userKey}`).set(update);
-      document.getElementById('newTxt').value = '';
-    }
-  });
+        } else {
+            const userKey = firebase.database().ref().child('Mensajes').push().key;
+            let update = {
+                user: actUser.uid,
+                userName: actUser.displayName,
+                post: postTxt
+            };
+            firebase.database().ref(`Mensajes/${userKey}`).set(update);
+            document.getElementById('newTxt').value = '';
+        }
+    });
 };
 
 const postByUser = (user) => {
-  const confirmUser = firebase.auth().currentUser;
-  runUser = confirmUser.displayName;
-  sesionUser.innerHTML = 'Hola' + ' ' + runUser;
+    const confirmUser = firebase.auth().currentUser;
+    runUser = confirmUser.displayName;
+    sesionUser.innerHTML = 'Hola' + ' ' + runUser;
 };
 
 const userPorfile = () => {
-  firebase.auth().onAuthStateChanged(checkStatusUser = (user) => {
-    if (user) {
-      let runName = user.displayName;
-      let runEmail = user.email;
-      let runPhoto = user.photoURL || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
-      userName.textContent = runName;
-      userEmail.textContent = runEmail;
-      profileImage.setAttribute('src', runPhoto + '?type=large');
-    }
-    postByUser();
-  });
+    firebase.auth().onAuthStateChanged(checkStatusUser = (user) => {
+        if (user) {
+            let runName = user.displayName;
+            let runEmail = user.email;
+            let runPhoto = user.photoURL || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
+            userName.textContent = runName;
+            userEmail.textContent = runEmail;
+            profileImage.setAttribute('src', runPhoto + '?type=large');
+        }
+        postByUser();
+    });
 
-  firebase.database().ref('Mensajes')
-    .on('child_added', (userKey)=>{
-      wallPost.innerHTML +=
-    `<div id="wallPost" class="card publication">
+    firebase.database().ref('Mensajes')
+        .on('child_added', (userKey) => {
+
+
+            wallPost.innerHTML +=
+                `<div id="wallPost" class="card publication">
       <div  class="card-body">
         <p>${userKey.val().userName}</p>
         <p>${userKey.val().post}</p>
         <div class="text-right">
-        <a  class="btn btn-outline-info" onclick="editPost"><i class="fas fa-thumbs-up"></i></a>
+        <a  class="btn btn-outline-info" onclick="likePost"><i class="fas fa-thumbs-up"></i></a>
         <a  class="btn btn-outline-secondary" onclick="editPost"><i class="fas fa-edit"></i></a>
         <a class="btn btn-outline-danger" onclick="deletPost"><i class="fas fa-trash-alt"></i></a>
         </div>
       </div>
     </div>`;
-    });
+        });
 
 };
 
 
 
 window.onload = function() {
-  postCommit();
-  userPorfile();
+    postCommit();
+    userPorfile();
 };
