3f21bc0d55d348fc7581d702f63682d258b2d4a7
FeerRivera <feerkay15@gmail.com>
2018-08-01 01:50:27 -0500

    cambio en el wall

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
 // Registro Usuarios Nuevos
 
 const btnSignUpModal = document.getElementById('btnSignUpModal');
@@ -76,41 +72,9 @@ firebase.auth().onAuthStateChanged(function(user) {
   } else {
     console.log('No hay usuario activo');
 
-<<<<<<< HEAD
-=======
-window.userRegister = {
-
-//registro nuevo
-newUser: (emailSignUp, passwordSignUp) => {
-    firebase.auth().createUserWithEmailAndPassword(emailSignUp, passwordSignUp)
-      .then(result => {
-        const user = firebase.auth().currentUser;
-        alert('Hola corredor!');
-        location.href = ('views/wall.html');
-      })
-      .catch (e => alert(e.message))
-  },
-
-//login con usuario registrado
-loginEmail: (emailLogin, passLogin) => {
-    if(emailLogin != '' && passLogin != '') {
-      firebase.auth().signInWithEmailAndPassword(emailLogin, passLogin)
-        .then(result => {
-          location.href = ('views/wall.html');
-        })
-        .catch (e => alert(e.message));
-    } else {
-      alert('Ingresa tu correo y contraseña para continuar');
-    }
-  },
-
-
-//login con google
-loginGoogle: () => {
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
     // No user is signed in.
   }
-},
+});
 
 
 // login
@@ -129,22 +93,9 @@ window.userRegister = {
     }
   },
 
-<<<<<<< HEAD
 
   loginFacebook: () => {
     if (!firebase.auth().currentUser) {
-=======
-
-
-  loginFacebook: () => {
-    if (!firebase.auth().currentUser) {
-
-//login con facebook
-loginFacebook: () => {
-
-if (!firebase.auth().currentUser) {
-
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
       const provider = new firebase.auth.FacebookAuthProvider();
       provider.addScope('public_profile');
       firebase.auth().signInWithPopup(provider).then(result => {
@@ -156,11 +107,7 @@ if (!firebase.auth().currentUser) {
     }
   },
 
-<<<<<<< HEAD
   loginGitHub: () => {
-=======
- loginGitHub: () => {
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
     if (!firebase.auth().currentUser) {
       const provider = new firebase.auth.GithubAuthProvider();
       provider.addScope('repo');
@@ -168,47 +115,8 @@ if (!firebase.auth().currentUser) {
         location.href = ('../views/wall.html');
       })
         .catch(e => alert(e.message));
-<<<<<<< HEAD
-=======
-
-//login con github
-loginGitHub: () => {
-if (!firebase.auth().currentUser) {
-    const provider = new firebase.auth.GithubAuthProvider();
-    provider.addScope('repo');
-    firebase.auth().signInWithPopup(provider).then(result => {
-      location.href = ("../src/views/wall.html");
-    })
-    .catch(e => alert(e.message));
-
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
     } else {
       firebase.auth().signOut();
     }
   },
-<<<<<<< HEAD
-=======
-
-const userData = firebase.auth().currentUser;
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: " + profile.providerId);
-    console.log("  Provider-specific UID: " + profile.uid);
-    console.log("  Name: " + profile.displayName);
-    console.log("  Email: " + profile.email);
-    console.log("  Photo URL: " + profile.photoURL);
-  });
-},
-
-//cerrar sesión
-  signOut: () => {
-   firebase.auth().signOut()
-     .then(element => {
-       location.href('../index.html');
-     }).catch(error => {
-       console.log('Error paracerrar sesión');
-     });
- }
-
->>>>>>> bf8ec5f7e1575901e561f875003e89dad846e36d
 };
