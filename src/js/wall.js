af6ec1fa885126a695946523a4a5a2e2807c2885
CarisAvi21 <shinnytinystar@hotmail.com>
2018-08-01 08:22:01 -0500

    login

--- src/js/wall.js
+++ src/js/wall.js
@@ -1,61 +1,59 @@
-let inputTxt = document.getElementById("newTxt");
-let newPost = document.getElementById("newPost");
-let wallPost = document.getElementById("wallPost");
-let deletePost = document.getElementById("deletePost");
-let editPost = document.getElementById("editPost");
+let inputTxt = document.getElementById('newTxt');
+let newPost = document.getElementById('newPost');
+let wallPost = document.getElementById('wallPost');
+let deletePost = document.getElementById('deletePost');
+let editPost = document.getElementById('editPost');
 
 
-let sesionUser = document.getElementById("sesionUser");
-let userName = document.getElementById("userName");
-let userEmail = document.getElementById("userEmail");
-let profileImage = document.getElementById("photo-user");
+let sesionUser = document.getElementById('sesionUser');
+let userName = document.getElementById('userName');
+let userEmail = document.getElementById('userEmail');
+let profileImage = document.getElementById('photo-user');
 
 
 let runUser;
 
 const postCommit = () => {
-    newPost.addEventListener('click', saveData = () => {
-        const actUser = firebase.auth().currentUser;
-        const postTxt = inputTxt.value;
-        if (postTxt == '') {
+  newPost.addEventListener('click', saveData = () => {
+    const actUser = firebase.auth().currentUser;
+    const postTxt = inputTxt.value;
+    if (postTxt == '') {
 
-        } else {
-            const userKey = firebase.database().ref().child('Mensajes').push().key;
-            let update = {
-                user: actUser.uid,
-                userName: actUser.displayName,
-                post: postTxt
-            };
-            firebase.database().ref(`Mensajes/${userKey}`).set(update);
-            document.getElementById('newTxt').value = '';
-        }
-    });
+    } else {
+      const userKey = firebase.database().ref().child('Mensajes').push().key;
+      let update = {
+        user: actUser.uid,
+        userName: actUser.displayName,
+        post: postTxt
+      };
+      firebase.database().ref(`Mensajes/${userKey}`).set(update);
+      document.getElementById('newTxt').value = '';
+    }
+  });
 };
 
 const postByUser = (user) => {
-    const confirmUser = firebase.auth().currentUser;
-    runUser = confirmUser.displayName;
-    sesionUser.innerHTML = 'Hola' + ' ' + runUser;
+  const confirmUser = firebase.auth().currentUser;
+  runUser = confirmUser.displayName;
+  sesionUser.innerHTML = 'Hola' + ' ' + runUser;
 };
 
 const userPorfile = () => {
-    firebase.auth().onAuthStateChanged(checkStatusUser = (user) => {
-        if (user) {
-            let runName = user.displayName;
-            let runEmail = user.email;
-            let runPhoto = user.photoURL || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
-            userName.textContent = runName;
-            userEmail.textContent = runEmail;
-            profileImage.setAttribute('src', runPhoto + '?type=large');
-        }
-        postByUser();
-    });
+  firebase.auth().onAuthStateChanged(checkStatusUser = (user) => {
+    if (user) {
+      let runName = user.displayName;
+      let runEmail = user.email;
+      let runPhoto = user.photoURL || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
+      userName.textContent = runName;
+      userEmail.textContent = runEmail;
+      profileImage.setAttribute('src', runPhoto + '?type=large');
+    }
+    postByUser();
+  });
 
-    firebase.database().ref('Mensajes')
-        .on('child_added', (userKey) => {
-
-
-            wallPost.innerHTML +=
+  firebase.database().ref('Mensajes')
+    .on('child_added', (userKey) => {
+      wallPost.innerHTML +=
                 `<div id="wallPost" class="card publication">
       <div  class="card-body">
         <p>${userKey.val().userName}</p>
@@ -67,13 +65,11 @@ const userPorfile = () => {
         </div>
       </div>
     </div>`;
-        });
-
+    });
 };
 
 
-
 window.onload = function() {
-    postCommit();
-    userPorfile();
+  postCommit();
+  userPorfile();
 };
